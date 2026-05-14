b51c831829555a968ddf7dc1431b511687225115
Oduntan David <davidoduntan1@gmail.com>
2026-05-14 15:18:37 +0100

    data cleaned

--- services/data_service.py
+++ services/data_service.py
@@ -1,27 +1,24 @@
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
+
 class DataService:
     def __init__(self, repository):
         self.repository = repository
-        self.model = None
+        self.scaler = StandardScaler()
 
     def inspect_data(self):
         data = self.repository.load_data()
-        # print("First 5 rows of the dataset:\n", data.head())
-        # print("Data Shape:", data.shape)
+
         print("Data Types:\n", data.dtypes)
-        # print("Missing Values:\n", data.isnull().sum())
-        # print("Class Distribution:\n", data['Class'].value_counts())
 
         return data
-    
-    
+
     def prepare_data(self):
         data = self.repository.load_data()
 
-        print('Missing values in dataset:\n', data.isnull().sum())
-        print('duplicate rows in dataset:', data.duplicated().sum())
+        print("Missing values in dataset:\n", data.isnull().sum())
+        print("Duplicate rows in dataset:", data.duplicated().sum())
 
         data = data.drop_duplicates()
         data = data.fillna(0)
@@ -29,11 +26,9 @@ class DataService:
         features = data.drop('Class', axis=1)
         target = data['Class']
 
-        scaler = StandardScaler()
-
         X = features.copy()
 
-        X[['Time', 'Amount']] = scaler.fit_transform(
+        X[['Time', 'Amount']] = self.scaler.fit_transform(
             X[['Time', 'Amount']]
         )
 
@@ -47,8 +42,7 @@ class DataService:
             stratify=y
         )
 
-        print("Training set shape:", x_train.shape)
-        print("Testing set shape:", x_test.shape)
+        print(f"Training set shape: {x_train.shape}")
+        print(f"Testing set shape: {x_test.shape}")
 
         return x_train, x_test, y_train, y_test
-        
